f051e96bbd82711267a0b5ddb025215ba18d4fa9
Rubio45 <aleshdiaz45@gmail.com>
2024-10-18 09:22:32 -0600

    Inicio del proyecto

---
+++ Ejercicio1Guia/Program.cs
@@ -0,0 +1,14 @@
+﻿/*
+Programa que sea capaz de almacenar los datos de 10 personas: nombre, dirección,
+   teléfono, edad (usando structs). Deberá ir pidiendo los datos uno por uno, hasta que
+   el usuario lo decida. Entonces deberá aparecer un menú que permita:
+   • Mostrar la lista de todos los nombres.
+   • Mostrar las personas de una cierta edad.
+   • Mostrar las personas que coincidan con un nombre. Sea el que el usuario indique.
+   • Salir del programa.
+   - Cada opción del menú representa una función o procedimiento que se invocará desde
+   la clase principal.
+   - Utilizar funciones o procedimientos con parámetros.
+   */
+   
+   
